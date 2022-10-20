290a49bc5143f0b1a68b27ac79606fe3ab592e3e
AviArta <buzlova.a@yandex.ru>
2022-10-20 19:29:17 +0300

    HW-1

--- 1.1-first-project/first_project/app/views.py
+++ 1.1-first-project/first_project/app/views.py
@@ -1,19 +1,17 @@
+import datetime
+from os import listdir
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
 
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
     pages = {
-        'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Главная страница': '/',
+        'Показать текущее время': '/current_time/',
+        'Показать содержимое рабочей директории': '/workdir/'
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -21,15 +19,12 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
-    # возвращается просто текст
     current_time = None
-    msg = f'Текущее время: {current_time}'
+    msg = f'Текущее время: {datetime.datetime.now().time()}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
-    # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
-    # директории
+    result = listdir()
+    return HttpResponse(result)
     raise NotImplemented
--- 1.1-first-project/first_project/first_project/urls.py
+++ 1.1-first-project/first_project/first_project/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from app.views import home_view
-
+from app.views import home_view, workdir_view, time_view
 
 urlpatterns = [
     path('', home_view, name='home'),
