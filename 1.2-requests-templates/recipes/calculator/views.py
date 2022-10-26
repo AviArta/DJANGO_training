0633f2ec5c19f6ff740754f4a172f46860c4c920
AviArta <buzlova.a@yandex.ru>
2022-10-26 16:14:51 +0300

    HW-2.1

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 DATA = {
@@ -28,3 +29,32 @@ DATA = {
 #     'ингредиент2': количество2,
 #   }
 # }
+
+
+def get_omlet(request):
+    servings = request.GET.get('servings', 1)
+    context = {'recipe': {
+        'яйца, шт': 2 * int(servings),
+        'молоко, л': 0.1 * int(servings),
+        'соль, ч.л.': 0.5 * int(servings)},
+        'servings': servings}
+    return render(request, 'calculator/index.html', context)
+
+def get_pasta(request):
+    servings = request.GET.get('servings', 1)
+    context = {'recipe': {
+        'макароны, г': 0.3 * int(servings),
+        'сыр, г': 0.05 * int(servings)},
+        'servings': servings}
+    return render(request, 'calculator/index.html', context)
+
+# реализовала через указание персон через параметр URL:
+def get_buter(request, counter):
+    context = {'recipe': {
+        'хлеб, ломтик': 1 * counter,
+        'колбаса, ломтик': 1 * counter,
+        'сыр, ломтик': 1 * counter,
+        'помидор, ломтик': 1 * counter,
+    },
+        'counter': counter}
+    return render(request, 'calculator/buter.html', context)
